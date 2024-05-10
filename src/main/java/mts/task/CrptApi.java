4ddde53d01fdb0c57bd42113566c9bccd2f9ce25
Mark <101831381+LilacShark@users.noreply.github.com>
2024-05-10 22:46:02 +0700

    Минорные правки

--- src/main/java/mts/task/CrptApi.java
+++ src/main/java/mts/task/CrptApi.java
@@ -99,6 +99,7 @@ public class CrptApi {
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             SomeResponse someResponse = gson.fromJson(response.body(), SomeResponse.class);
+            System.out.println(someResponse);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -143,110 +144,6 @@ class Document {
         this.reg_number = "reg_number";
         products.add(new Product());
     }
-
-    public Description getDescription() {
-        return description;
-    }
-
-    public void setDescription(Description description) {
-        this.description = description;
-    }
-
-    public String getDoc_id() {
-        return doc_id;
-    }
-
-    public void setDoc_id(String doc_id) {
-        this.doc_id = doc_id;
-    }
-
-    public String getDoc_status() {
-        return doc_status;
-    }
-
-    public void setDoc_status(String doc_status) {
-        this.doc_status = doc_status;
-    }
-
-    public DocType getDoc_type() {
-        return doc_type;
-    }
-
-    public void setDoc_type(DocType doc_type) {
-        this.doc_type = doc_type;
-    }
-
-    public boolean isImportRequest() {
-        return importRequest;
-    }
-
-    public void setImportRequest(boolean importRequest) {
-        this.importRequest = importRequest;
-    }
-
-    public String getOwner_inn() {
-        return owner_inn;
-    }
-
-    public void setOwner_inn(String owner_inn) {
-        this.owner_inn = owner_inn;
-    }
-
-    public String getParticipant_inn() {
-        return participant_inn;
-    }
-
-    public void setParticipant_inn(String participant_inn) {
-        this.participant_inn = participant_inn;
-    }
-
-    public String getProducer_inn() {
-        return producer_inn;
-    }
-
-    public void setProducer_inn(String producer_inn) {
-        this.producer_inn = producer_inn;
-    }
-
-    public LocalDate getProduction_date() {
-        return production_date;
-    }
-
-    public void setProduction_date(LocalDate production_date) {
-        this.production_date = production_date;
-    }
-
-    public String getProduction_type() {
-        return production_type;
-    }
-
-    public void setProduction_type(String production_type) {
-        this.production_type = production_type;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public LocalDate getReg_date() {
-        return reg_date;
-    }
-
-    public void setReg_date(LocalDate reg_date) {
-        this.reg_date = reg_date;
-    }
-
-    public String getReg_number() {
-        return reg_number;
-    }
-
-    public void setReg_number(String reg_number) {
-        this.reg_number = reg_number;
-    }
 }
 
 enum DocType {
@@ -259,23 +156,15 @@ class Description {
     public Description() {
         this.participantInn = "participantInn";
     }
-
-    public String getParticipantInn() {
-        return participantInn;
-    }
-
-    public void setParticipantInn(String participantInn) {
-        this.participantInn = participantInn;
-    }
 }
 
 class Product {
     private String certificate_document;
-    private LocalDate certificate_document_date = LocalDate.of(2020, 01, 23);
+    private LocalDate certificate_document_date = LocalDate.of(2020, 1, 23);
     private String certificate_document_number;
     private String owner_inn;
     private String producer_inn;
-    private LocalDate production_date = LocalDate.of(2020, 01, 23);
+    private LocalDate production_date = LocalDate.of(2020, 1, 23);
     private String tnved_code;
     private String uit_code;
     private String uitu_code;
@@ -290,77 +179,6 @@ class Product {
         this.uitu_code = "uitu_code";
     }
 
-    public void setCertificate_document(String certificate_document) {
-        this.certificate_document = certificate_document;
-    }
-
-    public void setCertificate_document_date(LocalDate certificate_document_date) {
-        this.certificate_document_date = certificate_document_date;
-    }
-
-    public void setCertificate_document_number(String certificate_document_number) {
-        this.certificate_document_number = certificate_document_number;
-    }
-
-    public void setOwner_inn(String owner_inn) {
-        this.owner_inn = owner_inn;
-    }
-
-    public void setProducer_inn(String producer_inn) {
-        this.producer_inn = producer_inn;
-    }
-
-    public void setProduction_date(LocalDate production_date) {
-        this.production_date = production_date;
-    }
-
-    public void setTnved_code(String tnved_code) {
-        this.tnved_code = tnved_code;
-    }
-
-    public void setUit_code(String uit_code) {
-        this.uit_code = uit_code;
-    }
-
-    public void setUitu_code(String uitu_code) {
-        this.uitu_code = uitu_code;
-    }
-
-    public String getCertificate_document() {
-        return certificate_document;
-    }
-
-    public LocalDate getCertificate_document_date() {
-        return certificate_document_date;
-    }
-
-    public String getCertificate_document_number() {
-        return certificate_document_number;
-    }
-
-    public String getOwner_inn() {
-        return owner_inn;
-    }
-
-    public String getProducer_inn() {
-        return producer_inn;
-    }
-
-    public LocalDate getProduction_date() {
-        return production_date;
-    }
-
-    public String getTnved_code() {
-        return tnved_code;
-    }
-
-    public String getUit_code() {
-        return uit_code;
-    }
-
-    public String getUitu_code() {
-        return uitu_code;
-    }
 }
 
 class SomeResponse {
@@ -368,22 +186,6 @@ class SomeResponse {
     private String state;
 
     public LocalDate responseDate;
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public LocalDate getResponseDate() {
-        return responseDate;
-    }
-
-    public void setResponseDate(LocalDate responseDate) {
-        this.responseDate = responseDate;
-    }
 
     @Override
     public String toString() {
--- src/main/java/mts/task/Program.java
+++ src/main/java/mts/task/Program.java
@@ -1,22 +1,19 @@
 package mts.task;
 
-import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 public class Program {
 
-    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+    public static void main(String[] args) {
 
         CrptApi crptApi = new CrptApi(TimeUnit.MINUTES, 10, 2);
 
         Thread taskThread = crptApi.startTaskCreatorThread();
         crptApi.semaphoreReleasingThread();
 
-        for (int i = 1; i <= 2; i++) {
+        for (int i = 1; i <= 1000; i++) {
             crptApi.setToSend(new Document(), "Signature " + i);
         }
-
-
     }
 
 }
